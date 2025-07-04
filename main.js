84a5458854ede8e3bdfdbef07e8efacbe81a5dc6
shafaq2005 <saqibbutt11917@gmail.com>
2025-07-04 18:21:54 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// output
+console.log("Hello")
+ alert("hi how are you")
+ confirm("Are You Good")
+// input
+var name = prompt("Enter Your Name")
+var age = null;
+console.log(name)
+// Data types
+// Text String "ali", "karachi", "hi how are you"
+// Number 1 ,1.1, -1, 12.121
+// 0,1 True False Boolean
+// khali , null
+// Object {id:1, name:"ali", fatherName:"bilal", active: true} key value pair
+// array fruits = ["apple", "banana", "mango", "cherry", "grapes", "guava"] strings
+// arry  name = ["sheharyar", "sameer", "muskan", "shafaq"] strings
+
+
+
